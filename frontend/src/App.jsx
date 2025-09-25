87406b188be769c32e74911d9ec4c5d6aa7e6572
Yash09042004 <yashkiran2004@gmail.com>
2025-09-25 16:57:24 +0530

    feat: add frontend and backend leaderboard app with MongoDB support

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Leaderboard from './Leaderboard'
+
+export default function App(){
+  return (
+    <div className="app">
+      <h1>Wargames Leaderboard</h1>
+      <Leaderboard />
+    </div>
+  )
+}
